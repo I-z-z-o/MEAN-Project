0392b50325484a6be9a51fb1638ee90a38eb3910
Ihsan Sensoy <isaak-one@hotmail.de>
2019-02-01 14:09:00 +0100

    Initial Commit

---
+++ backend/models/post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+//_id will be automatically created
+const postSchema = mongoose.Schema({
+
+  title: {
+    type: String,
+    required: true },
+
+  content: {
+    type: String,
+    required: true}
+
+});
+
+
+module.exports = mongoose.model('Post', postSchema);
+
